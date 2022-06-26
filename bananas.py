c06f47155b12bd11a06240dd9b50e13081f37fc9
tsyganno <tsyganlee@gmail.com>
2022-06-26 23:05:41 +0300

    fix pep8

--- bananas.py
+++ bananas.py
@@ -17,8 +17,24 @@ def bananas(s, word='banana'):
 
 assert bananas("banann") == set()
 assert bananas("banana") == {"banana"}
-assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
-                     "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
-                     "-ban--ana", "b-anana--"}
+assert bananas("bbananana") == {
+    "b-an--ana",
+    "-banana--",
+    "-b--anana",
+    "b-a--nana",
+    "-banan--a",
+    "b-ana--na",
+    "b---anana",
+    "-bana--na",
+    "-ba--nana",
+    "b-anan--a",
+    "-ban--ana",
+    "b-anana--"}
 assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
-assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
+assert bananas("bananana") == {
+    "ban--ana",
+    "ba--nana",
+    "bana--na",
+    "b--anana",
+    "banana--",
+    "banan--a"}
