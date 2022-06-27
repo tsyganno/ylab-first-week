d4675d69f309eb583033fe2643512af5a6acdc6b
tsyganno <tsyganlee@gmail.com>
2022-06-27 22:12:53 +0300

    fix first task

--- domain_name.py
+++ domain_name.py
@@ -11,9 +11,15 @@ def domain_name(url):
         first_index = url.find('www') + 4
         second_index = url[first_index:].find('.') + first_index
         return url[first_index: second_index]
+    else:
+        index = url.find('.')
+        return url[:index]
 
 
 assert domain_name("http://google.com") == "google"
 assert domain_name("http://google.co.jp") == "google"
 assert domain_name("www.xakep.ru") == "xakep"
 assert domain_name("https://youtube.com") == "youtube"
+assert domain_name("http://github.com/carbonfive/raygun") == "github"
+assert domain_name("http://www.zombie-bites.com") == "zombie-bites"
+assert domain_name("https://www.cnet.com") == "cnet"
